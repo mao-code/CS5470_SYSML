695502fc1aed7574dc6482f8aee5cd5cb286da35
MaoXun <a22908352@gmail.com>
2025-10-02 10:10:38 -0400

    adjust command

--- assignment_2/client.sh
+++ assignment_2/client.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 benchmark.py --backend openai --model meta-llama/CodeLlama-34b-hf --request-rate 3 --num-prompts 30 --dataset-name dummy --long-prompts 0 --long-prompt-len 32000
+python3 benchmark.py --backend openai --model meta-llama/CodeLlama-34b-hf --request-rate 3 --num-prompts 50 --dataset-name dummy --long-prompts 0 --long-prompt-len 32000
